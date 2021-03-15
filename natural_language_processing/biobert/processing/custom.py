4f9315b0297958ce20b63f2e00f57400775d9987
Lukasz Ruczynski <lukasz.ruczynski@netguru.com>
2021-03-15 17:11:18 +0100

    update

--- natural_language_processing/biobert/processing/custom.py
+++ natural_language_processing/biobert/processing/custom.py
@@ -34,6 +34,7 @@ class Tokenizer:
             return_attention_mask=True,
             return_token_type_ids=True,  # check if necessary
             return_offsets_mapping=True,
+            max_length=10,
         )
 
         sequence_ids = tokenized_qa.sequence_ids()
@@ -69,3 +70,103 @@ class Tokenizer:
             'start_position': start_position,
             'end_position': end_position
         }
+
+
+inputs = tokenizer.encode_plus(example.question_text, example.context_text, return_tensors='pt')
+start_logits, end_logits = model(**inputs)
+
+
+def to_list(tensor):
+    return tensor.detach().cpu().tolist()
+
+
+start_logits = to_list(start_logits)[0]
+end_logits = to_list(end_logits)[0]
+
+start_idx_and_logit = sorted(enumerate(start_logits), key=lambda x: x[1], reverse=True)
+end_idx_and_logit = sorted(enumerate(end_logits), key=lambda x: x[1], reverse=True)
+
+print(start_idx_and_logit[:5])
+print(end_idx_and_logit[:5])
+
+start_indexes = [idx for idx, logit in start_idx_and_logit[:5]]
+end_indexes = [idx for idx, logit in end_idx_and_logit[:5]]
+
+
+tokens = to_list(inputs['input_ids'])[0]
+
+question_indexes = [i+1 for i, token in enumerate(tokens[1:tokens.index(102)])]
+question_indexes
+
+import collections
+
+# keep track of all preliminary predictions
+PrelimPrediction = collections.namedtuple(
+    "PrelimPrediction", ["start_index", "end_index", "start_logit", "end_logit"]
+)
+
+prelim_preds = []
+for start_index in start_indexes:
+    for end_index in end_indexes:
+        # throw out invalid predictions
+        if start_index in question_indexes:
+            continue
+        if end_index in question_indexes:
+            continue
+        if end_index < start_index:
+            continue
+        prelim_preds.append(
+            PrelimPrediction(
+                start_index = start_index,
+                end_index = end_index,
+                start_logit = start_logits[start_index],
+                end_logit = end_logits[end_index]
+            )
+        )
+
+prelim_preds = sorted(prelim_preds, key=lambda x: (x.start_logit + x.end_logit), reverse=True)
+pprint(prelim_preds[:5])
+
+BestPrediction = collections.namedtuple(  # pylint: disable=invalid-name
+    "BestPrediction", ["text", "start_logit", "end_logit"]
+)
+
+nbest = []
+seen_predictions = []
+for pred in prelim_preds:
+
+    # for now we only care about the top 5 best predictions
+    if len(nbest) >= 5:
+        break
+
+    # loop through predictions according to their start index
+    if pred.start_index > 0:  # non-null answers have start_index > 0
+
+        text = tokenizer.convert_tokens_to_string(
+            tokenizer.convert_ids_to_tokens(
+                tokens[pred.start_index:pred.end_index + 1]
+            )
+        )
+        # clean whitespace
+        text = text.strip()
+        text = " ".join(text.split())
+
+        if text in seen_predictions:
+            continue
+
+            # flag this text as being seen -- if we see it again, don't add it to the nbest list
+            seen_predictions.append(text)
+
+            # add this text prediction to a pruned list of the top 5 best predictions
+            nbest.append(BestPrediction(text=text, start_logit=pred.start_logit, end_logit=pred.end_logit))
+
+        # and don't forget -- include the null answer!
+        nbest.append(BestPrediction(text="", start_logit=start_logits[0], end_logit=end_logits[0]))
+
+"""
+The null answer is scored as the sum of the start_logit and end_logit associated with the [CLS] token.
+"""
+
+"""
+https://qa.fastforwardlabs.com/no%20answer/null%20threshold/bert/distilbert/exact%20match/f1/robust%20predictions/2020/06/09/Evaluating_BERT_on_SQuAD.html#Evaluating-a-model-on-the-SQuAD2.0-dev-set-with-HF
+"""
